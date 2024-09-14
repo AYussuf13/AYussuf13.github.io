60a6b46e0afb2740fce987a7111e49f980175eb3
AYussuf13 <amalh.yussuf@gmail.com>
2024-09-14 17:35:35 +0100

    MedSmart

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-document.addEventListener("DOMContentLoaded",() => {
+document.addEventListener('DOMContentLoaded', function () {
+    new Swiper('.card-wrapper', {
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable:true,
+        dynamicBullets:true
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints:{
+        0:{
+          slidesPerView:1
+        },
+        768:{
+          slidesPerView:2
+        },
+        1024:{
+          slidesPerView:3
+        },
+      }
   
-    const images = document.querySelectorAll("img");  
+    });
+  });
   
-    for(const image of images){
-      fetch("https://dog.ceo/api/breeds/image/random")
-      .then(response => response.json())
-      .then (data =>{
-           image.src = data.message
-           image.width = 100;
-           image.height = 100;
-    })
-    }
-  
-  
-  })
